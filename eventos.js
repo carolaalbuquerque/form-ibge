f851ab36bc03995fe85b9ba3fa219626b81bcaef
Carol Albuquerque <dev.carolalbuquerque@gmail.com>
2022-09-19 19:55:19 -0300

    main

---
+++ eventos.js
@@ -0,0 +1,41 @@
+const URL_REGIOES = 'https://servicodados.ibge.gov.br/api/v1/localidades/regioes';
+
+const urlEstados = (id) =>`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${id}/estados`;
+
+const urlCidades = (id) => `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${id}/municipios`;
+
+fetch(URL_REGIOES)
+    .then(response => response.json())
+    .then(data => {
+        data.map(cadaResult => {
+            regiao.innerHTML += `<option value="${cadaResult.id}"> ${cadaResult.nome} </option>`;
+        });
+    });
+
+regiao.addEventListener('change', () => {
+    estado.innerHTML = `<option>--Selecione--</option>`
+
+    fetch(urlEstados(regiao.value))
+    .then(response => response.json())
+    .then(dados => { 
+        dados.map(est => {
+            estado.innerHTML += `<option value="${est.id}">${est.nome}</option>`
+        })
+    });
+});
+
+estado.addEventListener(`change`, () => {
+    fetch
+})
+
+cidade.addEventListener('change', () => {
+    cidade.innerHTML = `<option>--Selecione--</option>`
+    fetch(urlCidades)
+    .then(response => response.json())
+    .then(dados => {
+        dados.map(cid => {
+            cidade.innerHTML += `<option>${cid.nome}</option>`
+        })
+    }); //select2
+})
+
